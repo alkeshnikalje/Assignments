718d69d9ce63b8be071d2252fd7d2fe21f4160d3
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-08-09 17:32:59 +0530

    added new input size in addproduct route and console logged the req body

--- remainingTasks/index.js
+++ remainingTasks/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("inside middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use("/addproduct",(req,res,next)=>{
+    res.send("<form action = '/product' method = 'POST'><input type = 'text' name = 'title'><input type = 'text' name = 'size'><button type = 'submit'>Send</button></form>");
 })
 
-app.use((req,res,next)=>{
-    console.log("inside another middleware");
+
+app.post("/product",(req,res)=>{
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
 })
+
+app.use("/",(req,res,next)=>{
+    res.send("<h1>hello from express</h1>");
+})
+
+
 
 app.listen(3000);
